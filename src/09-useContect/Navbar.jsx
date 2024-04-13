91b65d83db1e4aa1a11c310cd3a7c108bf9f6c51
Juan17Informatico <juancampuzano2356@gmail.com>
2024-04-12 20:24:42 -0500

    Section 11 - Clase 160 finalizada

--- src/09-useContect/Navbar.jsx
+++ src/09-useContect/Navbar.jsx
@@ -1,11 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <>
-            <Link to="/">Home</Link>
-            <Link to="/about">about</Link>
-            <Link to="/login">login</Link>
-        </>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <Link className="navbar-brand" href="/">UseContext</Link>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="/"
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="about"
+                        >
+                            About
+                        </NavLink>
+                        
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="login"
+                        >
+                            Login
+                        </NavLink>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
